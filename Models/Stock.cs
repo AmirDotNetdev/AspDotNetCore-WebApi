f2e9ffe79014dfa1418500b799ab36a1ba34e558
amirdotnetdev <amirkamyabi4@gmail.com>
2024-03-17 17:20:58 +0330

    commit

---
+++ Models/Stock.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comment { get; set; } = new List<Comment>();
+
+
+    }
+}
